ce793298f5b750beb8ca55372b885d29bca7548a
Jose Carlos M. Ramirez <al15211330@ite.edu.mx>
2022-05-18 00:24:35 -0700

    Modules
    Added modules and optimized some features on the index page using a template.

--- 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/views.py
+++ 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/views.py
@@ -2,18 +2,44 @@ from itertools import count
 from django import views
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
 
-    context = {
-        'name': 'Patrick',
-        'age': 23,
-        'nationality': 'Mexican'
-    }
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.isTrue = True
+    feature1.details = 'Our service is really quick'
 
-    name = "Charlie"
-    return render(request, 'index.html', context)
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.isTrue = True
+    feature2.details = 'Our service is really Reliable'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to Use'
+    feature3.isTrue = False
+    feature3.details = 'Our service is really is Easy to Use'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Affordable'
+    feature4.isTrue = True
+    feature4.details = 'Our service is really Affordable'
+
+    feature5 = Feature()
+    feature5.id = 4
+    feature5.name = 'Tarustwhorthy'
+    feature5.isTrue = True
+    feature5.details = 'Our service is filled with trust'
+
+    features = [feature1, feature2, feature3, feature4, feature5]
+
+    return render(request, 'index.html', {'features': features })
 
 def counter(request):
 
