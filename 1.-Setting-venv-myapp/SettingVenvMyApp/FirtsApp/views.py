2c9e62faadbc4387214061b2e0a5b736f05a2787
Jose Carlos M. Ramirez <al15211330@ite.edu.mx>
2022-05-17 20:25:54 -0700

    Word counter
    Created a basic word counter in Django using POST method. Changed the GET method to get rid of URL string.

--- 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/views.py
+++ 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/views.py
@@ -1,7 +1,23 @@
+from itertools import count
 from django import views
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hey, welcome to first django app </h1>")
+
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationality': 'Mexican'
+    }
+
+    name = "Charlie"
+    return render(request, 'index.html', context)
+
+def counter(request):
+
+    text = request.POST["text"]
+    numberOfWords = len(text.split())
+
+    return render(request, 'counter.html', {'amount': numberOfWords})
