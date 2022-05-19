d2e9604055e186fbf0cfaf6f6b9dd3e5794ed6e1
Jose Carlos M. Ramirez <al15211330@ite.edu.mx>
2022-05-18 22:17:14 -0700

    Finished project
    Finished working with django basics:
    
    > Register user
    > Login and logout
    > Dynamic URL routing
    > Modules
    > Admin Panel and DB manipulation

--- 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/models.py
+++ 1.-Setting-venv-myapp/SettingVenvMyApp/FirtsApp/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Feature():
+class Feature(models.Model):
 
-   id: int
-   name: str
-   details: str 
-   isTrue: bool
+   name = models.CharField(max_length = 100)
+   details = models.CharField(max_length = 500)
--- 1.-Setting-venv-myapp/SettingVenvMyApp/SettingVenvMyApp/settings.py
+++ 1.-Setting-venv-myapp/SettingVenvMyApp/SettingVenvMyApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'FirtsApp'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'SettingVenvMyApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'postgres',
+        'PASSWORD': 'Hola_1234',
+        'HOST': 'localhost'
     }
 }
 
